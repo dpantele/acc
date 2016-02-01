7504cadd517ef51b67fc1289f3fccc1eaf9c9177
Dmitry Panteleev <github@dpantele.com>
2016-02-01 15:58:49 -0500

    Small helper for endomorhpisms

--- crag/compressed_word/endomorphism.h
+++ crag/compressed_word/endomorphism.h
@@ -38,9 +38,32 @@ class Endomorphism
     }
   }
 
+  //! Applies @p a to every image
+  constexpr Endomorphism& RightMultiplyBy(const Endomorphism& a) {
+    for(auto& w : mapping_) {
+      w = a.Apply(w);
+    }
+    return *this;
+  }
+
+  friend std::ostream& operator<<(std::ostream& out, const Endomorphism& e) {
+    out << "(";
+    PrintWord(e.mapping_[XYLetter('x').AsInt()], &out);
+    out << ",";
+    PrintWord(e.mapping_[XYLetter('y').AsInt()], &out);
+    out << ")";
+    return out;
+  }
+
  private:
   CWord mapping_[2 * CWord::kAlphabetSize];
 };
+
+inline Endomorphism operator*(const Endomorphism& a, const Endomorphism& b) {
+  Endomorphism result = a;
+  result.RightMultiplyBy(b);
+  return result;
+}
 
 }
 
